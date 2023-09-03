bb4488facce8c1795216f1547ea7cf4c1c0335e5
Maxim Donchuk <m.donchuk@gmail.com>
2023-09-03 21:41:52 +0300

    Добавлен докстринг к MyTable

--- models/my_table.py
+++ models/my_table.py
@@ -4,6 +4,10 @@ from models.vacancy import Vacancy
 
 
 class MyTable(PrettyTable):
+    """Обертка над классом PrettyTable для удобного добавления вакансий.
+    Добавляет метод add_vacancies, который добавляет в таблицу данные
+    в нужном формате.
+    """
     def add_vacancies(self, vacancies: list[Vacancy]) -> None:
         """Принимает список вакансий и добавляет их в таблицу"""
 
