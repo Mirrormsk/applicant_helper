1ffeb15a9b63424c108184e9d26ba61189319b32
Maxim Donchuk <m.donchuk@gmail.com>
2023-09-04 10:55:15 +0300

    фикстуры переписаны с учетом разного id

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,6 +18,7 @@ def some_vacancy():
     )
     return vacancy
 
+
 @pytest.fixture
 def some_vacancy_2():
     vacancy = Vacancy(
@@ -35,9 +36,26 @@ def some_vacancy_2():
     return vacancy
 
 @pytest.fixture
+def some_vacancy_3():
+    vacancy = Vacancy(
+        id=3,
+        name="Тестовая вакансия",
+        salary_from=140_000,
+        salary_to=190_000,
+        currency="rub",
+        area="Тестовый город",
+        full_address="Г.Тестовый, ул Тестовая, 2",
+        url="https://test.ru",
+        requirements="Тестовый текст обязанностей в <highlighttext>вакансии номер 3</highlighttext>",
+        employer="Тестовый работодатель",
+    )
+    return vacancy
+
+
+@pytest.fixture
 def some_vacancy_without_salary_from():
     vacancy = Vacancy(
-        id=2,
+        id=4,
         name="Тестовая вакансия",
         salary_from=None,
         salary_to=190_000,
@@ -50,10 +68,11 @@ def some_vacancy_without_salary_from():
     )
     return vacancy
 
+
 @pytest.fixture
 def some_vacancy_without_salary_to():
     vacancy = Vacancy(
-        id=2,
+        id=5,
         name="Тестовая вакансия",
         salary_from=140_000,
         salary_to=None,
@@ -62,6 +81,56 @@ def some_vacancy_without_salary_to():
         full_address="Г.Тестовый, ул Тестовая, 2",
         url="https://test.ru",
         requirements="Тестовый текст обязанностей в <highlighttext>вакансии номер 3</highlighttext>",
+        employer="Тестовый работодатель",
+    )
+    return vacancy
+
+
+@pytest.fixture
+def vacancy_python_php():
+    vacancy = Vacancy(
+        id=6,
+        name="Тестовая вакансия python php",
+        salary_from=100_000,
+        salary_to=None,
+        currency="rub",
+        area="Тестовый город",
+        full_address="Г.Тестовый, ул Тестовая, 2",
+        url="https://test.ru",
+        requirements="Python, php",
+        employer="Тестовый работодатель",
+    )
+    return vacancy
+
+
+@pytest.fixture
+def vacancy_python_java_go():
+    vacancy = Vacancy(
+        id=7,
+        name="Тестовая вакансия 10",
+        salary_from=100_000,
+        salary_to=None,
+        currency="rub",
+        area="Тестовый город",
+        full_address="Г.Тестовый, ул Тестовая, 2",
+        url="https://test.ru",
+        requirements="Python, Java, Go",
+        employer="Тестовый работодатель",
+    )
+    return vacancy
+
+@pytest.fixture
+def vacancy_python_java():
+    vacancy = Vacancy(
+        id=8,
+        name="Тестовая вакансия 10",
+        salary_from=100_000,
+        salary_to=None,
+        currency="rub",
+        area="Тестовый город",
+        full_address="Г.Тестовый, ул Тестовая, 2",
+        url="https://test.ru",
+        requirements="Python, Java",
         employer="Тестовый работодатель",
     )
     return vacancy
