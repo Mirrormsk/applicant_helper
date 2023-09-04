cd06c265b6edfc25ea51c9b5ce0dfc486257aeb7
Maxim Donchuk <m.donchuk@gmail.com>
2023-09-04 10:02:29 +0300

    дополнен сценарий пользовательской работы

--- main.py
+++ main.py
@@ -7,9 +7,32 @@ import config
 from utils.table import table
 
 
+from utils import actions
+
+
 def main():
     # Список для дальнейшего хранения вакансий
     all_vacancies = []
+
+    input(
+        "Вас приветствует приложение для удобного сбора вакансий с разных платформ!\n"
+        "Нажмите enter, чтобы продолжить\n"
+    )
+
+    # Проверка, на присутствие в переменных среды API-ключа для SuperJob.
+    # При отсутствии ключа, SuperJob не будет предложен в качестве платформы.
+    sj_api_key = os.getenv("SJ_API_KEY")
+
+    if not sj_api_key:
+        user_input = input(
+            '\nВ Вашем окружении отсутствует переменная "SJ_API_KEY" с API-ключом для SuperJob.\n'
+            "Для использования API SuperJob, закройте это приложение (CTRL + C), и добавьте Ваш API-ключ командой \n"
+            "export SJ_API_KEY={ваш API-ключ}\n"
+            "Без ключа поиск на этой платформе не будет произведен.\n"
+            "Нажмите enter, чтобы продолжить..."
+        )
+
+        del config.PARSERS["SuperJob"]
 
     # Начальный выбор платформ для сбора вакансий
     while True:
@@ -63,21 +86,73 @@ def main():
             saver.clear()
             print("Старые данные удалены.\n")
 
+    # Добавить и сохранить все вакансии в файле
     saver.add_vacancies(all_vacancies)
 
-    vacancies = saver.get_vacancies_by_salary(100_000, 150_000)
+    # Запрос на фильтрацию по зарплате
+    if enquiries.confirm("Отфильтровать вакансии по зарплате?"):
+        vacancies = actions.filter_by_salary(saver)
+    else:
+        vacancies = all_vacancies
+
+    # Запрос на фильтрацию по ключевым словам
+    if enquiries.confirm("Отфильтровать вакансии по ключевым словам?"):
+        filter_words = input("Введите слова для поиска через пробел -> ").split()
+        vacancies = actions.filter_vacancies(vacancies, filter_words)
+
+    # Запрос на сортировку
+    if enquiries.confirm("Отсортировать вакансии по зарплате?"):
+        vacancies = actions.sort_vacancies(vacancies)
 
     # Добавляем данные в таблицу
-
     table.add_vacancies(vacancies)
 
-    # print(table)
+    # Вывод данных
+    if not vacancies:
+        print("Не нашлось вакансий с выбранными критериями")
+    else:
+        print(table)
 
-    chosen_vacancy = vacancies[
-        int(input("Ввыдите номер вакансии для выбора действия -> ")) - 1
-    ]
+        # Цикл работы с вакансиями индивидуально
 
-    print(chosen_vacancy)
+        while True:
+            user_vacancy_choice = input("\nВведите номер вакансии для выбора действия -> ")
+
+            if not user_vacancy_choice.isdigit() or not 1 <= int(
+                user_vacancy_choice
+            ) <= len(vacancies):
+                print(f"\nНомер должен быть целым числом от 1 до {len(vacancies)}")
+                continue
+
+            chosen_vacancy = vacancies[int(user_vacancy_choice) - 1]
+
+            vacancy_actions = {
+                "Посмотреть полную информацию": "review",
+                "Удалить вакансию": "delete",
+                "Выбрать другую вакансию": "continue",
+                "Закончить работу": "exit",
+            }
+
+            user_action = enquiries.choose(
+                "\nВыберете дальнейшее действие с вакансией",
+                vacancy_actions,
+                multi=False,
+            )
+
+            if user_action == "review":
+                print(chosen_vacancy)
+            elif user_action == "delete":
+                saver.delete_vacancy(chosen_vacancy)
+                vacancies.remove(chosen_vacancy)
+                table.clear()
+                table.add_vacancies(vacancies)
+                print(table)
+            elif user_action == "continue":
+                continue
+            elif user_action == "exit":
+                break
+
+    print("\nПриложение завершает работу. Хорошего дня!")
 
 
 if __name__ == "__main__":
